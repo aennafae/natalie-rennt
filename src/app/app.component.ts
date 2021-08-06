cb90b0a9ed3f8a9ebb7363124c12dd3a1ab08cea
Anna <annafaerber@hotmail.de>
2021-08-06 09:29:43 +0200

    Fourth stamp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ export class AppComponent implements OnInit {
   showLoadMoreButton: boolean = false;
 
   // Current point to start in runs[] Array to count the km for gauge
-  startingPointOfRunCalculation = 128;
+  startingPointOfRunCalculation = 169;
 
   runsFirst: Run[] = [];
   runsLeft: Run[] = [];
